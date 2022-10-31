bcc5ddf756054a000d611cd23559cae0ab343a26
threehero <2206563880@qq.com>
2022-11-01 02:45:15 +0800

    登录功能完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import employee from '@/views/employee'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    redirect: '/employee',
+    name: 'employee',
+    component: employee
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/employee',
+    name: 'employee',
+    component: employee
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "about" */ '@/views/login')
+  },
+  {
+    path: '/404',
+    name: '/404'
+  },
+  {
+    path: '*',
+    redirect: '/404'
   }
 ]
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 修改端口
+  devServer: {
+    host: '127.0.0.1',
+    port: '8000'
+  },
+  // 修改标题
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = '醉仙楼'
+      return args
+    })
+  }
 })
